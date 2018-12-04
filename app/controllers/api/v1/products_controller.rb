46954de7219df4d6f10ec8bc7c1f4d0c57fe662e
Lu <luacosta.fc@gmail.com>
2018-12-03 21:28:47 -0500

    Adding update and delete

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -11,9 +11,24 @@ module Api
         end
       end
 
-      def destroy
+      def edit
         @product = Product.find(params[:id])
-        @product.destroy
+      end
+
+      def update
+        product = Product.find(params[:id])
+        if product.update(product_params)
+          render json: product, status: 200
+        else
+          render json: {errors: product.errors}, status: 422
+        end
+      end
+
+      def destroy
+        product =Product.find(params[:id])
+        if product.destroy
+          render json: {}, status: 204
+        end
       end
 
 
